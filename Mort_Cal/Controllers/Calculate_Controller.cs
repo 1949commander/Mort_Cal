d83a39102b7a9e8113a20aa08d3d5b3cf8c0d67c
Brian Reeves <1949commander@gmail.com>
2021-07-20 00:45:35 -0700

    Add project files.

---
+++ Mort_Cal/Controllers/Calculate_Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mort_Cal.Controllers
+{
+    public class Calculate_Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            var calculate = new Calculate_Controller();
+            return View(calculate);
+        }
+    }
+}
---
+++ Mort_Cal/Models/Calculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mort_Cal.Models
+{
+    public class Calculate
+    {
+        public decimal amount { get; set; }
+        public decimal interest { get; set; }
+        public int period { get; set;  }
+        public decimal total_amt { get; set; }
+        public decimal monthly_payment { get; set; }
+    }
+}
